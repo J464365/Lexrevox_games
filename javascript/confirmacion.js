417b2a9f33e3d0f820bd30afa314232c0ede406e
J464365 <oviedorico95@gmail.com>
2025-11-09 14:57:45 -0500

    Add files via upload

---
+++ javascript/confirmacion.js
@@ -0,0 +1,79 @@
+function c1() {
+  if (window.confirm("¿Estás seguro que quieres donar $1.000?")) {
+
+    alert("¡Nos donastes $1.000!, muchas gracias");
+  } else {
+
+    alert("la donacion fue cancelada acción cancelada.");
+  }
+}
+
+function c2() {
+  if (window.confirm("¿Estás seguro que quieres donar $5.000?")) {
+
+    alert("¡Nos donastes $5.000!, muchas gracias");
+  } else {
+
+    alert("la donacion fue cancelada acción cancelada.");
+  }
+}
+
+function c3() {
+  if (window.confirm("¿Estás seguro que quieres donar $10.000?")) {
+
+    alert("¡Nos donastes $10.000!, muchas gracias");
+  } else {
+
+    alert("la donacion fue cancelada acción cancelada.");
+  }
+}
+
+function c4() {
+  if (window.confirm("¿Estás seguro que quieres donar $50.000?")) {
+
+    alert("¡Nos donastes $50.000!, muchas gracias");
+  } else {
+
+    alert("la donacion fue cancelada acción cancelada.");
+  }
+}
+
+function c5() {
+  if (window.confirm("¿Estás seguro que quieres donar $100.000?")) {
+
+    alert("¡Nos donastes $100.000!, muchas gracias");
+  } else {
+
+    alert("la donacion fue cancelada acción cancelada.");
+  }
+}
+
+function c6() {
+  if (window.confirm("¿Estás seguro que quieres donar $500.000?")) {
+
+    alert("¡Nos donastes $500.000!, muchas gracias");
+  } else {
+
+    alert("la donacion fue cancelada acción cancelada.");
+  }
+}
+
+function c7() {
+  if (window.confirm("¿Estás seguro que quieres donar $1.000.000?")) {
+
+    alert("¡Nos donastes $1.000.000!, muchas gracias");
+  } else {
+
+    alert("la donacion fue cancelada acción cancelada.");
+  }
+}
+
+function c8() {
+  if (window.confirm("¿Estás seguro que quieres donar $5.000.000?")) {
+
+    alert("¡Nos donastes $5.000.000!, muchas gracias");
+  } else {
+
+    alert("la donacion fue cancelada acción cancelada.");
+  }
+}
